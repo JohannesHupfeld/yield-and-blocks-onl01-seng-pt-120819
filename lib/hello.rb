30304024b75c36444d952eac3a6461ab932b262f
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-12-23 02:54:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,17 +1,11 @@
-def hello_t
-if block_given?
-     i = 0
-      while i < 
- array.length
-       yield array[i]
-       i = i + 1
-     end
-
-      array
-   else
-     puts "Hey! No block was given!"
-   end
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
 
-
-
+hello(list) {|i| "Hello #{i} what's up?"}
